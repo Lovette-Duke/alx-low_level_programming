882838f94333082db9981c63d1d55f7956375031
Lovette Duke <qutyluv@gmail.com>
2023-08-15 22:59:51 +0100

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - holds the statements that displays _putchar
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
