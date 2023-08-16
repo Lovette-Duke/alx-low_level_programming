6c1f2a9dd2afb8937bca05a60522cb2dfc5b3f1f
Lovette Duke <qutyluv@gmail.com>
2023-08-16 03:11:54 +0100

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: int to be added
+ * @j: int to be added
+ * Return: addition
+ */
+
+int add(int i, int j)
+{
+	int plus = i + j;
+
+	return (plus);
+}
