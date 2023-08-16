e475a49b66beefe7e5d371f38f6138e84eae0487
Lovette Duke <qutyluv@gmail.com>
2023-08-16 03:49:31 +0100

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all number from n to 98
+ * @n: the beginning number to count from
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("98\n");
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("98\n");
+	}
+	else
+	{
+		printf("98\n");
+	}
+}
