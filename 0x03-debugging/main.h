339f0a27798149a4ac66662c478b2d8b7017a94e
Lovette Duke <qutyluv@gmail.com>
2023-08-17 00:00:51 +0100

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define main_h
 #include <stdio.h>
 
-void positive_or_negetive(int i);
+void positive_or_negative(int i);
 int largest_numer(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
