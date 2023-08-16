797a260b5baf0a48c8797377580bb2cfc7eff980
Lovette Duke <qutyluv@gmail.com>
2023-08-16 01:19:01 +0100

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: the number which it last digit will be printed
+ * Return: last digit
+ */
+
+int print_last_digit(int i)
+{
+	int ld = i % 10;
+
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
