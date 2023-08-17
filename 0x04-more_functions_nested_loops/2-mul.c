fa655b59ede77fcc6d690c0a37ca8060f407b736
Lovette Duke <qutyluv@gmail.com>
2023-08-17 19:06:52 +0100

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multipies 2 integers
+ * @a: an argument to be multiplied
+ * @b: the 2nd argument to be multiplied
+ * Return: result of the multiplied values
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
