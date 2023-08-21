b99ed9efaaca44c1cce687df2e8f5d97961892b3
Lovette Duke <qutyluv@gmail.com>
2023-08-21 22:20:46 +0100

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
