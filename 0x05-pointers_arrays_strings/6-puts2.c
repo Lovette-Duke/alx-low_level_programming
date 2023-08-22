c3b5460905278e1ec535be26d6fa6fcc44045a75
Lovette Duke <qutyluv@gmail.com>
2023-08-22 05:18:36 +0100

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints alternating characters
+ * @str: the string from which we get alternating chars
+ * Returns: none
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
