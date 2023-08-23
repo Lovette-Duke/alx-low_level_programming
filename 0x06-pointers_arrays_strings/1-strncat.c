36b68d87248d1a2e4dec86cd99e5706d9b74beae
Lovette Duke <qutyluv@gmail.com>
2023-08-23 21:01:12 +0100

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings but has a byte limit
+ * @dest: the destination string
+ * @src: the string to be concatenated
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = '\0';
+	return (s);
+}
