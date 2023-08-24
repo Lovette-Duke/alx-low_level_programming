a94e585ffcc8514906ce0656ebb2044a74d9a0b1
Lovette Duke <qutyluv@gmail.com>
2023-08-24 23:34:45 +0100

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - returns an integer deferenced by p
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -16,7 +20,7 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-	*(p + 5) = 98;
+	*n = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
