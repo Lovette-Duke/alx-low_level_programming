03482f1c28d59b0e37e163cb081d8945d55ee9a7
Lovette Duke <qutyluv@gmail.com>
2023-08-28 23:16:45 +0100

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,11 +11,20 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
+	int j;
 
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
-			return (&haystack[i]);
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
 		i++;
 	}
 	return (NULL);
