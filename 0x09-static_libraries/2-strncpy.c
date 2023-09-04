5161945a4a8b23d2b698753c979a442b43fd3f45
Lovette Duke <qutyluv@gmail.com>
2023-09-04 06:59:05 +0100

    Task 0

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination of copied string
+ * @src: string to be copied
+ * @n: the byte limit of string to be copied
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This folder holds the tasks for the Static library project
