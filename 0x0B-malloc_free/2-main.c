578abd812552a2e6509f089a3322a71c55ed563b
Lovette Duke <qutyluv@gmail.com>
2023-09-05 13:30:05 +0100

    Task 2

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -10,14 +10,18 @@
 int main(void)
 {
 	    char *s;
+	    char *s1;
 
 	        s = str_concat("Betty ", "Holberton");
+		s1 = str_concat("Hello", NULL);
 		    if (s == NULL)
 			        {
 					        printf("failed\n");
 						        return (1);
 							    }
 		        printf("%s\n", s);
+			printf("%s\n", s1);
 			    free(s);
+			    free(s1);
 			        return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,13 +16,16 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	if (s1 != NULL && s2 != NULL)
-	{
-		while (s1[len1] != '\0')
-			len1++;
-		while (s2[len2] != '\0')
-			len2++;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
 	concat_s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (concat_s == NULL)
