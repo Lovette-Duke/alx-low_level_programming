d045885053e7c5ed6f3a651d93b2791e6073315c
Lovette Duke <qutyluv@gmail.com>
2023-09-08 01:47:13 +0100

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,19 +11,17 @@
 int *array_range(int min, int max)
 {
 	int *array;
-	int i = 0;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	array = malloc(sizeof(int) * (max - min) + 1);
+	array = malloc(sizeof(int) * (max - min + 1));
 
 	if (array == NULL)
 		return (NULL);
 
-	while (min <= max)
-	{
+	for (i = 0; min <= max; i++)
 		array[i] = min++;
-		i++;
-	}
+
 	return (array);
 }
