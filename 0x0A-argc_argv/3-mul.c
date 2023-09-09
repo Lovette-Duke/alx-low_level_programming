3e0275b42507070dee087e3dac6a1fb57949e7dd
Lovette Duke <qutyluv@gmail.com>
2023-09-09 20:52:19 +0100

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,10 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
+	int mul;
 
-	(void) argc;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
+	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (0);
 }
