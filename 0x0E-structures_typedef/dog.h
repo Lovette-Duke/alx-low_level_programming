d1bc1db943e064ea816a2a52c89de4e1c5bfdd57
Lovette Duke <qutyluv@gmail.com>
2023-09-12 21:03:41 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the struct dog
+ * @name: the name of the dog
+ * @age: the dog's age
+ * @owner: the name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
