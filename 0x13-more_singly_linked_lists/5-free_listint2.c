6eb356f9c49a9361a91b24079260efac0af9d558
Lovette Duke <qutyluv@gmail.com>
2023-09-25 21:57:53 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees up the space allocated by malloc
+ * @head: the pointer to the pointer to the head of the linked list
+ * Return: none
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+}
