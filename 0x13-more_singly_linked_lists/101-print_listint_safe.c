d21e9cb7e1d90961fc1f9852fa608957da908a4e
Lovette Duke <qutyluv@gmail.com>
2023-09-28 14:15:26 +0100

    lists.h correction

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list
+ * @head: the head of the linked list
+ * Return: the number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	
+}
