0c741684f23c34ebfad8e293432babede72e00eb
Lovette Duke <qutyluv@gmail.com>
2023-09-29 11:17:19 +0100

    Task 10 correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * git_bit - produces the value of a bit at a given index
+ * get_bit - produces the value of a bit at a given index
  * @n: the interger to be converted
  * @index: the index of the bit to be returned
  * Return: the value of the bit at index or -1
