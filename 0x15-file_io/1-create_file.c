8b085ee1697e9fa25b1a198e8f128daeb00b6764
Lovette Duke <qutyluv@gmail.com>
2023-10-09 12:23:17 +0100

    Task 1 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i]; i++)
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
 			i++;
-	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(file, text_content, i);
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that apoends text
+ * filename: the name of the file
+ * text_content: the string to be added to the file
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i;
+	int w;
+	int len = 0;
+
+	if (filename == NULL)
+	{
+		
+	}
+}
