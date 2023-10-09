7b29e9c7c86abfc18b81c46c5fd726ce7e151147
Lovette Duke <qutyluv@gmail.com>
2023-10-09 13:25:02 +0100

    Task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 			i++;
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
 	w = write(file, text_content, i);
 
 	if (file == -1 || w == -1)
