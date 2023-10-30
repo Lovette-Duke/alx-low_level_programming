8f929ab5fe3af713c64d179438170d9bc2991e19
Lovette Duke <qutyluv@gmail.com>
2023-10-30 15:27:22 +0100

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file as the name implies
+ * @filename: the name ofthe file to be created
+ * @text_content: the string to write to the file
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int w;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+			i++;
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	w = write(file, text_content, i);
+
+	if (file == -1 || w == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+}
